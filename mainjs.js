48efaf5b4f9e17c367a5c5a486f89bd90609daa6
Lifestream308 <99936729+Lifestream308@users.noreply.github.com>
2022-09-13 18:51:14 -0700

    changing site to my profile site

--- mainjs.js
+++ mainjs.js
@@ -1,65 +1,80 @@
-// Javascript Pokemon website that takes in JSON from my own API
+// Javascript file for my personal Profile website
 
-let obj
+// Takes value of the input box and appends to a div. DOM manipulation.
+function addDescription() {
+    if (document.getElementById("descriptionInput").value == '') {
+        return alert("You need to enter text!")
+    }
+    getParagraph = document.createElement("p")
+    getParagraph.className = "relative"
+    description = document.getElementById("descriptionInput").value
+    getParagraph.append(description)
+    appendContainer = document.querySelector("#appendContainer")
+    appendContainer.append(getParagraph)
+    document.getElementById("descriptionInput").value = "";
 
-async function getText() {
-    let x = await fetch('https://pokemondb308.herokuapp.com/getposts');
-    let y = await x.text();
-    obj = JSON.parse(y)
-    console.log(obj)
-  }
-
-function chooseImages() {
-    input = document.getElementById('1stinput')
-    image = document.getElementById('1stimage')
-    image.src = 'images/' + input.value + '.gif'
-    title1 = document.getElementById('title1')
-    title1.innerHTML = obj[input.value - 1].title
-    content1 = document.getElementById('content1')
-    content1.innerHTML = obj[input.value - 1].content
-    number1 = document.getElementById('number1')
-    number1.innerHTML = '# ' + obj[input.value - 1].number
-    type1 = document.getElementById('type1')
-    type1.innerHTML = 'Type - ' + obj[input.value - 1].type
-    weakness1 = document.getElementById('weakness1')
-    weakness1.innerHTML = 'Weaknesses - ' + obj[input.value - 1].weakness
-
-
-    input = document.getElementById('2ndinput')
-    image = document.getElementById('2ndimage')
-    image.src = 'images/' + input.value + '.gif'
-    title2 = document.getElementById('title2')
-    title2.innerHTML = obj[input.value - 1].title
-    content2 = document.getElementById('content2')
-    content2.innerHTML = obj[input.value - 1].content
-    number2 = document.getElementById('number2')
-    number2.innerHTML = '# ' + obj[input.value - 1].number
-    type2 = document.getElementById('type2')
-    type2.innerHTML = 'Type - ' + obj[input.value - 1].type
-    weakness2 = document.getElementById('weakness2')
-    weakness2.innerHTML = 'Weaknesses - ' + obj[input.value - 1].weakness
-
-
-    input = document.getElementById('3rdinput')
-    image = document.getElementById('3rdimage')
-    image.src = 'images/' + input.value + '.gif'
-    title3 = document.getElementById('title3')
-    title3.innerHTML = obj[input.value - 1].title
-    content3 = document.getElementById('content3')
-    content3.innerHTML = obj[input.value - 1].content
-    number3 = document.getElementById('number3')
-    number3.innerHTML = '# ' + obj[input.value - 1].number
-    type3 = document.getElementById('type3')
-    type3.innerHTML = 'Type - ' + obj[input.value - 1].type
-    weakness3 = document.getElementById('weakness3')
-    weakness3.innerHTML = 'Weaknesses - ' + obj[input.value - 1].weakness
-}
-
-function hitEnter() {
-    if (event.key === "Enter") {
-        chooseImages()
+    // These lines added for the X close function
+    var span = document.createElement("span");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    getParagraph.appendChild(span);
+  
+    for (let i = 0; i < close.length; i++) {
+      close[i].onclick = function() {
+        var div = this.parentElement;
+        div.style.display = "none";
+      }
     }
    }
-//    figure.addEventListener("mousemove", zoom);
 
-getText()
+// Allows User to append input value by pressing "Enter"
+function hitEnter() {
+    if (event.key === "Enter") {
+        addDescription()
+    }
+   }
+   
+var i = 0; // initialize counter for typeWriter()
+var j = 0; // initialize counter for typeWriter2()
+   
+// Slowly types a word into the input box before calling addDescription()
+function typeWriter() {
+var txt = 'Dedicated.';
+if (i < txt.length) {
+document.getElementById("descriptionInput").value += txt.charAt(i);
+i++;
+    if (i >= txt.length) {
+    addDescription()
+    typeWriter2()
+    }
+setTimeout(typeWriter, 150);
+}
+}
+// Same as typeWriter(). Type word into input box then call addDescription()
+function typeWriter2() {
+    var txt = "Passionate."
+    if (j < txt.length) {
+    document.getElementById("descriptionInput").value += txt.charAt(j);
+    j++;
+        if (j >= txt.length) {
+        addDescription()
+        }
+    setTimeout(typeWriter2, 150);
+}
+}
+
+// Adds a onclick function to every element with class "close"
+// Click on a close button to hide the current list item
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
+
+// initiate function
+typeWriter()
